420f7d56c830167c69868cf66231579e41f130cb
davidgilson24~ <cs@david-gilson.com~>
2024-02-06 21:47:53 +0100

    COUNTER +

--- React-test/src/App.jsx
+++ React-test/src/App.jsx
@@ -1,16 +1,20 @@
 import './style.css'
+import { useState } from "react"
 
 export default function App(){
+  const[count, setCount] = useState(0)
+
   return(
     <>
-      <form className='new-item-form'>
-        <div className='form-row'>
-          <label htmlFor='item'> New item: </label>
-          <input id='item'/>
-        </div>
-        <button className='btn'> Add Task </button>
-      </form>
-      <h1 className='header'> Task List</h1>
+      <h1> The counter button! </h1>
+      <div className='btn-style'>
+        <button 
+          type='button'
+          onClick={() => setCount(count + 1)} 
+          id='btn'
+          > Increase !</button>
+      </div>
+      <h1 id='counter'> {count} </h1>
     </>
   )
 }
