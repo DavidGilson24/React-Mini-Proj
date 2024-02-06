1a3aa00bd762aa192d5063905d0dfa7a2b218666
davidgilson24~ <cs@david-gilson.com~>
2024-02-06 19:10:09 +0100

    task list made

--- React-test/src/App.jsx
+++ React-test/src/App.jsx
@@ -1,3 +1,16 @@
-export default function(){
-  
+import './style.css'
+
+export default function App(){
+  return(
+    <div>
+      <form className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor='item'> New item: </label>
+          <input id='item'/>
+        </div>
+        <button className='btn'> Add Task </button>
+      </form>
+      <h1 className='header'> Task List</h1>
+    </div>
+  )
 }
